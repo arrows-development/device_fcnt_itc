951fb709dcd4853dd94436fa062495c5337d94b7
kailua <kailua@project2by2.jp>
2025-07-14 00:28:42 +0900

    itc: 2by2 bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_itc.mk
+    $(LOCAL_DIR)/itc.mk
--- itc.mk
+++ itc.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/fcnt/itc/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common 2by2 stuff.
+$(call inherit-product, vendor/2by2/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
-PRODUCT_NAME := lineage_itc
+PRODUCT_NAME := itc
 PRODUCT_DEVICE := itc
 PRODUCT_MANUFACTURER := fcnt
 PRODUCT_BRAND := FCNT
